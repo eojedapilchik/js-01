daa44b0c180f786bf21827536bf431415ddface9
Alcides Ojeda <eojedapilchik@gmail.com>
2021-05-04 11:30:45 -0400

    js functions added for quotes

--- js/script.js
+++ js/script.js
@@ -1,33 +1,78 @@
+"use strict";
 /******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
+A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+/***
+ * @type {quotes[]} Array containing all the quotes
+ ***/
 
-/*** 
- * `quotes` array 
-***/
+const quotes = [
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall",
+    source: "Nelson Mandela",
+    citation: "Speech",
+    year: 1980,
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    source: "Walt Disney",
+    year: 1950,
+  },
+  {
+    quote:
+      "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+    source: "Steve Jobs",
+    citation: "Speech",
+    year: 2005,
+  },
+  {
+    quote:
+      "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+    source: "Oprah Winfrey",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    source: "John Lennon",
+  },
+];
 
+// console.log(quotes);
+/***
+ * Gets a random quote from the quotes array
+ *
+ * @return {object} A random quote
+ ***/
 
+function getRandomQuote() {
+  const index = Math.floor(Math.random() * quotes.length);
+  return quotes[index];
+}
 
 /***
- * `getRandomQuote` function
-***/
+ * Prints the Quote randomly selected to html elements
+ *
+ * @return {string} html Element with the quote and info to be added to the page
+ ***/
 
-
-
-/***
- * `printQuote` function
-***/
-
-
+function printQuote() {
+  const quote = getRandomQuote();
+  let html = `<p class="quote"> ${quote.quote} </p>
+              <p class="source"> ${quote.source} `;
+  if (quote.citation)
+    html += `<span class="citation"> ${quote.citation} </span>`;
+  if (quote.year) html += `<span class="year"> ${quote.year} </span>`;
+  html += "</p>";
+  document.getElementById("quote-box").innerHTML = html;
+  return html;
+}
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
+ ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document
+  .getElementById("load-quote")
+  .addEventListener("click", printQuote, false);
